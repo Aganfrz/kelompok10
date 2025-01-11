6470788e0f23954847cc5762bf8f335d74905794
aganfrz <aganfahrizal377@gmail.com>
2025-01-11 14:16:52 +0700

    penambahan produk

--- database/seeders/ProdukSeeder.php
+++ database/seeders/ProdukSeeder.php
@@ -13,54 +13,19 @@ class ProdukSeeder extends Seeder
         //
         $produks = [
             [
-                'nama' => 'Cheetos',
-                'deskripsi' => 'Kripik jagung gurih',
-                'harga' => 3000.00,                 
+                'nama' => 'Nescafe Gold',
+                'deskripsi' => 'Kopi nescafe',
+                'harga' => 10000.00,                 
             ],
             [
-                'nama' => 'Beng-beng',
-                'deskripsi' => 'Snack coklat',
-                'harga' => 2000.00,                
+                'nama' => 'Arabika',
+                'deskripsi' => 'Kopi arabika',
+                'harga' => 20000.00,                
             ],
             [
-                'nama' => 'Milku',
-                'deskripsi' => 'Minuman susu berkualitas',
-                'harga' => 3500.00,                
-            ],
-            [
-                'nama' => 'Taro',
-                'deskripsi' => 'Snack cemilan mantap, 1 bungkus 10g',
-                'harga' => 2000.00,                
-            ],
-            [
-                'nama' => 'Ultra Milk',
-                'deskripsi' => 'Minuman susu sehat, 200mil',
-                'harga' => 3500.00,                
-            ],
-            [
-                'nama' => 'Sampo Clear',
-                'deskripsi' => 'Sampo pembasmi ketombe, 1 renceng',
-                'harga' => 6000.00,                
-            ],
-            [
-                'nama' => 'Minyak 1Liter',
-                'deskripsi' => 'Minyak berkualitas tinggi',
-                'harga' => 19000.00,                
-            ],
-            [
-                'nama' => 'Buah Anggur',
-                'deskripsi' => 'Anggur muscat tanpa biji, 1Kg',
-                'harga' => 45000.00,                
-            ],
-            [
-                'nama' => 'Buah Mangga',
-                'deskripsi' => 'Mangga arum manis, 1kg',
-                'harga' => 22000.00,                
-            ],
-            [
-                'nama' => 'Roti',
-                'deskripsi' => 'Roti keju, satuan',
-                'harga' => 4000.00,                 
+                'nama' => 'Robusta',
+                'deskripsi' => 'Kopi robusta',
+                'harga' => 30000.00,                
             ],
         ];
 
